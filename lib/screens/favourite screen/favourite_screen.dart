87a544eeb5cf66a01cf350ab4309b793339cdab1
Suhaibusman <suhaibusman54@gmail.com>
2023-10-31 21:00:35 +0500

    doctor appointment app

--- lib/screens/favourite screen/favourite_screen.dart
+++ lib/screens/favourite screen/favourite_screen.dart
@@ -1,7 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:smithackathon/constants/colors.dart';
 import 'package:smithackathon/function/custom_function.dart';
+import 'package:smithackathon/screens/doctor_details.dart';
 import 'package:smithackathon/screens/navbar/bottomnavigation.dart';
+import 'package:smithackathon/widgets/textwidget.dart';
 
 class FavouriteScreen extends StatefulWidget {
   const FavouriteScreen({super.key});
@@ -29,14 +32,126 @@ class _FavouriteScreenState extends State<FavouriteScreen> {
         itemBuilder: (context, index) {
           DocumentSnapshot doc = favoriteDoctors[index];
           // Build the UI for displaying favorite doctors
-          return ListTile(
-            title: Text(doc["username"]),
-            // Add more widgets to display other doctor information
-          );
+          return Padding(
+                    padding: const EdgeInsets.only(left: 10, right: 10),
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => DoctorDetailsScreen(
+                                  username: doc["username"],
+                                  speciality: doc["speciality"],
+                                  profileimages: doc["picture"]),
+                            ));
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.white),
+                        width: MediaQuery.of(context).size.width,
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(20),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  CircleAvatar(
+                                    radius: 25,
+                                    backgroundImage:
+                                        NetworkImage(doc["profileimages"]),
+                                  ),
+                                  const Row(
+                                    children: [
+                                      Icon(
+                                        Icons.star_half_outlined,
+                                        color: MyColors.greenColor,
+                                      ),
+                                      Text("4.8"),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                TextWidget(
+                                    textMessage: doc["username"],
+                                    textColor: MyColors.blackColor,
+                                    textSize: 20),
+                                TextWidget(
+                                    textMessage: doc["speciality"],
+                                    textColor: MyColors.greyColor,
+                                    textSize: 13),
+                                Row(
+                                  children: [
+                                    Container(
+                                      height: 34,
+                                      width: 103,
+                                      decoration: BoxDecoration(
+                                          color: MyColors.greyColor
+                                              .withOpacity(0.3),
+                                          borderRadius:
+                                              BorderRadius.circular(10)),
+                                      child: const Center(
+                                          child: Text(
+                                        "Appointment",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold),
+                                      )),
+                                    ),
+                                    const SizedBox(
+                                      width: 10,
+                                    ),
+                                    Container(
+                                        height: 34,
+                                        width: 34,
+                                        decoration: BoxDecoration(
+                                            color: MyColors.greyColor
+                                                .withOpacity(0.3),
+                                            borderRadius:
+                                                BorderRadius.circular(10)),
+                                        child: const Center(
+                                          child: Icon(
+                                            Icons.chat,
+                                            color: MyColors.pinkColor,
+                                          ),
+                                        )),
+                                    const SizedBox(
+                                      width: 10,
+                                    ),
+                                    Container(
+                                        height: 34,
+                                        width: 34,
+                                        decoration: BoxDecoration(
+                                            color: MyColors.greyColor
+                                                .withOpacity(0.3),
+                                            borderRadius:
+                                                BorderRadius.circular(10)),
+                                        child:  Center(
+                                          child:IconButton(onPressed: (){
+                                                // addToFavorites(doc);
+                                          }, icon: const Icon(Icons.favorite,
+                                            color: MyColors.pinkColor,))
+                                        )),
+                                  ],
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                
         },
       );
     } else {
-      return Text('No favorite doctors found.');
+      return const Text('No favorite doctors found.');
     }
   },
 )
